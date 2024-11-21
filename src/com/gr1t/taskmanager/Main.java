84ed1829d61dcca52bc05e48295b874485020d10
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2024-11-21 21:48:43 +0200

    change user interface/add some methods

--- src/com/gr1t/taskmanager/Main.java
+++ src/com/gr1t/taskmanager/Main.java
@@ -1,27 +1,41 @@
 package com.gr1t.taskmanager;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Task Manager");
+        while (true) {
             System.out.println("1. Create Task");
-            System.out.println("2. View All Tasks");
-            System.out.println("3. View Tasks by Priority");
-            System.out.println("4. Update Task");
-            System.out.println("5. Mark Task as Completed");
-            System.out.println("6. Delete Task");
-            System.out.println("7. Exit");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Exit");
             System.out.print("Choose an option: ");
 
             int choice = scanner.nextInt();
             scanner.nextLine();
 
-
-
-
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter title: ");
+                    String title = scanner.nextLine();
+                    System.out.println("Enter description: ");
+                    String description = scanner.nextLine();
+                    System.out.println("Enter priority (LOW, MEDIUM, HIGH): ");
+                    Priority priority = Priority.valueOf(scanner.nextLine().toUpperCase());
+                    Task task = TaskManager.createTask(title, description, priority);
+                    System.out.println("Your task: " + task);
+                    break;
+                case 2:
+                    System.out.println("All tasks: ");
+                    TaskManager.printAllTasks();
+                    break;
+                case 3:
+                    System.out.println("Leave a review on zversilneykitayca.com");
+                    break;
+                default:
+                    System.out.println("clown?");
+            }
+        }
     }
 }
--- src/com/gr1t/taskmanager/Task.java
+++ src/com/gr1t/taskmanager/Task.java
@@ -1,15 +1,58 @@
 package com.gr1t.taskmanager;
 
 public class Task {
-    private int id;
+    private String title;
     private String description;
     private Priority priority;
-    private boolean isCompleted;
+    private boolean isCompleted = false;
 
-    public Task(boolean isCompleted, Priority priority, String description, int id) {
-        this.isCompleted = isCompleted;
-        this.priority = priority;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
-        this.id = id;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", priority=" + priority +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+
+    public Task(String title, String description, Priority priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+
+
     }
 }
