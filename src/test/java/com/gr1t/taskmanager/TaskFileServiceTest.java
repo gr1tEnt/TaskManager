e170eba4d9646058a8f31d8c5d2e3e6ab98f8779
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2024-12-25 17:59:42 +0200

    Add: Unit Test for TaskFileService for success saving

---
+++ src/test/java/com/gr1t/taskmanager/TaskFileServiceTest.java
@@ -0,0 +1,33 @@
+package com.gr1t.taskmanager;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskFileServiceTest {
+
+    private final String fileName = "testTasks";
+
+    @Test
+    public void testSaveTasksSuccess() throws IOException {
+        List<Task> tasks = Arrays.asList(
+                new Task("Task 1", "description 1", Priority.LOW),
+                new Task("Task 2", "description 2", Priority.LOW),
+                new Task("Task 3", "description 3", Priority.LOW)
+        );
+        TaskFileService.saveTasks(tasks, fileName);
+
+        Path filePath = Paths.get(fileName+ ".txt");
+        assertTrue(Files.exists(filePath), "The file should exist");
+        List<String> lines = Files.readAllLines(filePath);
+
+        assertEquals(3, lines.size(), "The file should contain 3 lines");
+    }
+
+}
